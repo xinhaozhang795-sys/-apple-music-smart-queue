502afc48552348e1bce5d3d7553d87ef03bed441
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-08-10 20:35:01 +0800

    feat: add Apple Music recently played provider

---
+++ Sources/SmartQueueMusicKit/ListeningHistoryProvider.swift
@@ -0,0 +1,19 @@
+import Foundation
+import MusicKit
+import SmartQueueCore
+
+public struct ListeningHistoryProvider: Sendable {
+    public init() {}
+
+    public func recentlyPlayedSongs() async throws -> [TrackCandidate] {
+        var request = MusicRecentlyPlayedContainerRequest<Song>()
+        request.limit = 50
+        let response = try await request.response()
+
+        return response.items.map { SongMapper().map($0, source: .recentlyPlayed) }
+    }
+
+    public func recentlyPlayedArtistNames() async throws -> Set<String> {
+        Set(try await recentlyPlayedSongs().map(\.artistName))
+    }
+}
