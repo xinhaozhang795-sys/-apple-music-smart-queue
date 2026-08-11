a303885a05fb3c4fcf058419089baf82fe0fbe91
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-08-11 14:08:24 +0800

    fix: request recently played songs with MusicKit song request

--- Sources/SmartQueueMusicKit/ListeningHistoryProvider.swift
+++ Sources/SmartQueueMusicKit/ListeningHistoryProvider.swift
@@ -6,7 +6,7 @@ public struct ListeningHistoryProvider: Sendable {
     public init() {}
 
     public func recentlyPlayedSongs() async throws -> [TrackCandidate] {
-        var request = MusicRecentlyPlayedContainerRequest()
+        var request = MusicRecentlyPlayedRequest<Song>()
         request.limit = 50
         let response = try await request.response()
 
