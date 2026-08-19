a12944cd7b38c78b37ee5294333f210c4260d839
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-08-19 08:54:22 +0800

    test: add observation target smoke test

---
+++ Tests/SmartQueueObservationTests/ModuleTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import SmartQueueObservation
+
+final class SmartQueueObservationModuleTests: XCTestCase {
+    func testModuleBoundaryCompiles() {
+        _ = SmartQueueObservationModule.self
+    }
+}
