9668e421430788f655a2d3bd7e26e600c8b4bb8a
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-08-19 08:54:01 +0800

    chore: add app target skeleton

---
+++ Sources/SmartQueueApp/Module.swift
@@ -0,0 +1,10 @@
+import SmartQueueCore
+import SmartQueueDomain
+import SmartQueueStorage
+import SmartQueueObservation
+import SmartQueueIntelligence
+import SmartQueueQueue
+import SmartQueueMusicKit
+
+// Application composition boundary. UI and lifecycle wiring will migrate here incrementally.
+enum SmartQueueAppModule {}
