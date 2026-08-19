c3e6db7645fe4d069484e91f225c01a311d4bef0
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-08-19 08:53:46 +0800

    chore: add intelligence target skeleton

---
+++ Sources/SmartQueueIntelligence/Module.swift
@@ -0,0 +1,6 @@
+import SmartQueueCore
+import SmartQueueDomain
+import SmartQueueStorage
+
+// Intelligence interprets evidence and produces decisions; it does not control playback.
+enum SmartQueueIntelligenceModule {}
