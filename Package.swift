9f80b6e490aead7ea15b4f8b6a49d6f80b7c0081
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-08-19 09:11:07 +0800

    ci: expose aggregate package product for integration validation

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     ],
     products: [
         .library(name: "SmartQueueCore", targets: ["SmartQueueCore"]),
-        .library(name: "SmartQueueMusicKit", targets: ["SmartQueueMusicKit"])
+        .library(name: "SmartQueueMusicKit", targets: ["SmartQueueMusicKit"]),
+        .library(name: "SmartQueueApp", targets: ["SmartQueueApp"])
     ],
     targets: [
         .target(
