c601320900b691e3897f6d02302c52fd8bc3704a
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-08-19 08:54:31 +0800

    test: add intelligence target smoke test

---
+++ Tests/SmartQueueIntelligenceTests/ModuleTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import SmartQueueIntelligence
+
+final class SmartQueueIntelligenceModuleTests: XCTestCase {
+    func testModuleBoundaryCompiles() {
+        _ = SmartQueueIntelligenceModule.self
+    }
+}
