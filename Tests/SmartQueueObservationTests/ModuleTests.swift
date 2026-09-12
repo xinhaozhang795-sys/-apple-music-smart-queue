f638babab274a9b4ddbc7299fb98b51f9551b26f
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-09-12 13:07:33 +0800

    refactor: remove obsolete observation module test

--- Tests/SmartQueueObservationTests/ModuleTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import SmartQueueObservation
-
-final class SmartQueueObservationModuleTests: XCTestCase {
-    func testModuleBoundaryCompiles() {
-        _ = SmartQueueObservationModule.self
-    }
-}
