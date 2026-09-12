2e0cf512f723f3cdd17529860fbc5a8f80da7b12
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-09-12 13:07:27 +0800

    refactor: remove obsolete intelligence module test

--- Tests/SmartQueueIntelligenceTests/ModuleTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import SmartQueueIntelligence
-
-final class SmartQueueIntelligenceModuleTests: XCTestCase {
-    func testModuleBoundaryCompiles() {
-        _ = SmartQueueIntelligenceModule.self
-    }
-}
