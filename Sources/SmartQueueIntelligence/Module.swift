72f37237ec0c4e328ab364be2993c7700f6e9463
xinhaozhang795-sys <xinhaozhang795@gmail.com>
2026-09-12 13:05:26 +0800

    refactor: remove premature intelligence layer

--- Sources/SmartQueueIntelligence/Module.swift
+++
@@ -1,6 +0,0 @@
-import SmartQueueCore
-import SmartQueueDomain
-import SmartQueueStorage
-
-// Intelligence interprets evidence and produces decisions; it does not control playback.
-enum SmartQueueIntelligenceModule {}
